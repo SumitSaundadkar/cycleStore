87680526ee359502a63e86a9eaababa1570d814c
Sumit K Saundadkar <sumit.saundadkar9010@gmail.com>
2022-03-29 18:35:22 +0530

    added react router dom to navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import {Navbar} from './Componets/NavBar/Navbar';
 import { HomePage } from "./Pages/HomePage";
+import { BrowserRouter, Routes, Route,Link} from "react-router-dom";
 
 
 function App() {
--- src/Componets/NavBar/Navbar.js
+++ src/Componets/NavBar/Navbar.js
@@ -1,4 +1,5 @@
 import './navbar.css';
+import {Link} from "react-router-dom";
 
 
 const Navbar = () => {
@@ -8,14 +9,16 @@ const Navbar = () => {
       <nav>
         <div className="logo">
           
-            <h4>
-              सायकलCYCLE!
-              <img
-                className="brand-img"
-                src="https://images.unsplash.com/photo-1523357585206-175e971f2ad9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80"
-                alt=""
-              />
-            </h4>
+           <Link to="/">
+           <h4>
+           Go-Wheel
+           <img
+             className="brand-img"
+             src="https://images.unsplash.com/photo-1523357585206-175e971f2ad9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80"
+             alt=""
+           />
+         </h4>
+           </Link>
           
         </div>
         <input className="search-data" type="text" placeholder="serach" />
@@ -30,11 +33,16 @@ const Navbar = () => {
             <i className="fas fa-heart"></i>
           </li>
           
+            <Link to="/Cart">
             <li>
             <span className="cart-icon">0</span>
+            
+            <i className="fal fa-shopping-cart"></i>
+            
 
-              <i className="fal fa-shopping-cart"></i>
+             
             </li>
+            </Link>
         
         </ul>
       </nav>
