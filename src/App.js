315190ccf9ba5cda7c2910f3eb16d3cf21bf650e
Sumit K Saundadkar <sumit.saundadkar9010@gmail.com>
2022-03-29 16:20:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import "./App.css";
-<<<<<<< HEAD
 import {Navbar} from './Componets/NavBar/Navbar';
 import { HomePage } from "./Pages/HomePage";
-=======
->>>>>>> 3be19327e92adbb4e840ae8f76e75dcc3d05415b
 
 
 function App() {
   return (
-<<<<<<< HEAD
    <div>
    <Navbar/>
    <HomePage/>
@@ -16,12 +12,6 @@ function App() {
     
     
   );
-=======
-    <div>
-    </div>
-   
-  )
->>>>>>> 3be19327e92adbb4e840ae8f76e75dcc3d05415b
 }
 
 export default App;
