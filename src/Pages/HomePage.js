bdff654b75567ef24b2b751bb9af295aea705120
Sumit K Saundadkar <sumit.saundadkar9010@gmail.com>
2022-03-29 19:47:27 +0530

    final page added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,9 +2,6 @@ import './home.css';
 const HomePage=()=>{
     return(
         <div className='home_page'>
-        <div><img src='https://images.unsplash.com/photo-1583467875263-d50dec37a88c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3ljbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt='cycle'/></div>
-        <div><img src='https://images.unsplash.com/photo-1583467875263-d50dec37a88c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3ljbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt='cycle'/></div>
-        <div><img src='https://images.unsplash.com/photo-1583467875263-d50dec37a88c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y3ljbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60' alt='cycle'/></div>
         <div className='shop_btn_center'>
         <button className="btn btn-primary">SHOP NOW</button>
         </div>
